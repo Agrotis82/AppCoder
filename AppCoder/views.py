3c96cca91a16cd2a947b9177659b1aa5caac47db
Agos <agostinafregossi@agoss-air.lan>
2022-04-03 11:43:14 -0300

    Subo los archivos ya creados

---
+++ AppCoder/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from AppCoder.models import Curso
+
+
+# Create your views here.
+def curso(self):
+
+    curso = Curso(nombre= "Desarrollo Web", camada="19889")
+    curso.save()
+    texto = f"---> Curso: {curso.nombre} Camada: {curso.camada}"
+    return HttpResponse(texto)
+
