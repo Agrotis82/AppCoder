70f482a999de8735d8b7304be7e5b53c826f431c
Agos <agostinafregossi@agoss-air.lan>
2022-04-03 21:07:18 -0300

    URLs en la app

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -11,3 +11,17 @@ from AppCoder.models import Curso
 #    texto = f"---> Curso: {curso.nombre} Camada: {curso.camada}"
 #    return HttpResponse(texto)
 
+def inicio(request):
+    return HttpResponse ('vista inicio')
+
+def cursos(request):
+    return HttpResponse ('vista cursos')
+
+def profesores(request):
+    return HttpResponse('vista profesores')
+
+def estudiantes(request):
+    return HttpResponse('vista estudiantes')
+
+def entregables(request):
+    return HttpResponse('vista entregables')
